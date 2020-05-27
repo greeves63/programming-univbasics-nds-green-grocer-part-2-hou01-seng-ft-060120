0d10e7aa615e04c28a7fc13a71a6587f10f72fcc
Gregory Reeves <gregoryreevesjr@gmail.com>
2020-05-27 04:11:49 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,5 +1,16 @@
 require_relative './part_1_solution.rb'
-
+def consolidate_cart(cart)
+  hash = {}
+  cart.each do |item_hash|
+    item_hash.each do |name, price_hash|
+      if hash[name].nil?
+        hash[name] = price_hash.merge({:count => 1})
+      else
+        hash[name][:count] += 1
+      end
+    end
+  end
+  hash
 
   
 end
